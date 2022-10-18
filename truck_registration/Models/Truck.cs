89120f96407af2fadf0d5c2830b0b470e2ee5d92
iago-g-gazzara <67912477+iago-g-gazzara@users.noreply.github.com>
2022-10-18 17:00:58 -0300

    Adicionar arquivos de projeto.

---
+++ truck_registration/Models/Truck.cs
@@ -0,0 +1,8 @@
+﻿namespace truck_registration.Models
+{
+    public class Truck
+    {
+        public long Id { get; set; }
+        public string Modelo { get; set; }
+    }
+}
---
+++ truck_registration/TruckContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using truck_registration.Models;
+
+namespace truck_registration
+{
+    public class TruckContext : DbContext
+    {
+        public DbSet<Truck> Truck { get; set; }
+        public TruckContext(DbContextOptions<TruckContext> options) : base(options)
+        {
+        }
+    }
+}
