bd6bce94d8cb7418400c7b250564c7baea1133a8
iago-g-gazzara <67912477+iago-g-gazzara@users.noreply.github.com>
2022-10-18 17:08:50 -0300

    check in

--- truck_registration/Pages/Index.cshtml.cs
+++ truck_registration/Pages/Index.cshtml.cs
@@ -1,15 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using truck_registration.Models;
 
 namespace truck_registration.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<Truck> Trucks { get; set; }
+        public Truck Truck { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+
+            Trucks = new List<Truck>();
+            Trucks.Add(new Truck
+            {
+                Id = 1,
+                Modelo = "tESTE"
+            });
+
+            Trucks.Add(new Truck
+            {
+                Id = 2,
+                Modelo = "Teste 2"
+            });
         }
 
         public void OnGet()
