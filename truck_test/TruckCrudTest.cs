b8a4ae1fe515f4c5f541315f3011a0385ba854ac
iago-g-gazzara <67912477+iago-g-gazzara@users.noreply.github.com>
2022-10-19 18:47:49 -0300

    test setup

---
+++ truck_test/TruckCrudTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using truck_register.Models;
+using truck_register.Repositories.Concrete;
+using truck_register.Repositories.Interfaces;
+
+namespace truck_test
+{
+    public class TruckCrudTest
+    {
+        private readonly ITruckRepository _truckRepository;
+
+        public TruckCrudTest()
+        {
+            var services = new ServiceCollection();
+            services.AddTransient<ITruckRepository, TruckRepository>();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            _truckRepository = serviceProvider.GetService<ITruckRepository>();
+        }
+
+        [Fact]
+        public void CheckIfInsertIsWorking()
+        {
+            var truck = new Truck();
+            truck.Id = _truckRepository.FetchAll().Last().Id + 1;
+            truck.AnoFabricacao = "2022";
+            truck.AnoModelo = "2022";
+            truck.Modelo = "FH";
+
+            var insertedTruck = _truckRepository.Insert(truck);
+
+            Assert.True(_truckRepository.GetById(insertedTruck.Id) != null);
+        }
+    }
+}
