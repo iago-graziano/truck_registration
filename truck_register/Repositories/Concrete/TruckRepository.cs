0ff8758f01c0dc6213f18e8d6d9cc8afe63ee4de
iago-g-gazzara <67912477+iago-g-gazzara@users.noreply.github.com>
2022-10-20 18:24:24 -0300

    final

--- truck_register/Repositories/Concrete/TruckRepository.cs
+++ truck_register/Repositories/Concrete/TruckRepository.cs
@@ -67,6 +67,7 @@ namespace truck_register.Repositories.Concrete
         {
             using (var context = new TruckContext())
             {
+                IsTruckModelValid(Truck);
                 Truck = context.Trucks.Add(Truck).Entity;
                 context.SaveChanges();
                 return Truck;
@@ -77,9 +78,30 @@ namespace truck_register.Repositories.Concrete
         {
             using (var context = new TruckContext())
             {
+                IsTruckModelValid(Truck);
                 Truck = context.Trucks.Update(Truck).Entity;
                 context.SaveChanges();
                 return Truck;
+            }
+        }
+
+        private void IsTruckModelValid(Truck truck)
+        {
+            var currentYear = DateTime.Now.Year;
+            if (Convert.ToInt32(truck.AnoFabricacao) != currentYear)
+            {
+                throw new Exception("O ano de fabricação deve ser o atual.");
+            }
+
+            var anoModelo = Convert.ToInt32(truck.AnoModelo);
+            if (anoModelo != currentYear && anoModelo != currentYear + 1)
+            {
+                throw new Exception("O ano modelo deve ser o atual ou o ano subsequente.");
+            }
+
+            if (truck.Modelo != "FM" && truck.Modelo != "FH")
+            {
+                throw new Exception("Modelo Inválido.");
             }
         }
     }
--- truck_test/TruckCrudTest.cs
+++ truck_test/TruckCrudTest.cs
@@ -54,5 +54,80 @@ namespace truck_test
 
             Assert.Equal(JsonConvert.SerializeObject(updatedTruck), JsonConvert.SerializeObject(truckToBeUpdated));
         }
+
+        [Fact]
+        public void InsertingInvalidTruckModelShouldThrowException()
+        {
+            var truck = new Truck();
+            truck.Id = _truckRepository.FetchAll().Last().Id + 1;
+            truck.AnoFabricacao = "2022";
+            truck.AnoModelo = "2022";
+            truck.Modelo = "auishdi";
+
+            var ex = Assert.Throws<Exception>(() => _truckRepository.Insert(truck));
+
+            Assert.Equal("Modelo Inválido.", ex.Message);
+        }
+
+        [Fact]
+        public void UpdatingInvalidTruckModelShouldThrowException()
+        {
+            var truckToBeUpdated = _truckRepository.FetchAll().First();
+            truckToBeUpdated.Modelo = "asdhuihi";
+
+            var ex = Assert.Throws<Exception>(() => _truckRepository.Update(truckToBeUpdated));
+
+            Assert.Equal("Modelo Inválido.", ex.Message);
+        }
+
+        [Fact]
+        public void InsertingInvalidTruckModelDateShouldThrowException()
+        {
+            var truck = new Truck();
+            truck.Id = _truckRepository.FetchAll().Last().Id + 1;
+            truck.AnoFabricacao = "2022";
+            truck.AnoModelo = "2015";
+            truck.Modelo = "FH";
+
+            var ex = Assert.Throws<Exception>(() => _truckRepository.Insert(truck));
+
+            Assert.Equal("O ano modelo deve ser o atual ou o ano subsequente.", ex.Message);
+        }
+
+        [Fact]
+        public void UpdatingInvalidTruckModelDateShouldThrowException()
+        {
+            var truckToBeUpdated = _truckRepository.FetchAll().First();
+            truckToBeUpdated.AnoModelo = "2015";
+
+            var ex = Assert.Throws<Exception>(() => _truckRepository.Update(truckToBeUpdated));
+
+            Assert.Equal("O ano modelo deve ser o atual ou o ano subsequente.", ex.Message);
+        }
+
+        [Fact]
+        public void InsertingInvalidTruckFabricationDateShouldThrowException()
+        {
+            var truck = new Truck();
+            truck.Id = _truckRepository.FetchAll().Last().Id + 1;
+            truck.AnoFabricacao = "2014";
+            truck.AnoModelo = "2022";
+            truck.Modelo = "FH";
+
+            var ex = Assert.Throws<Exception>(() => _truckRepository.Insert(truck));
+
+            Assert.Equal("O ano de fabricação deve ser o atual.", ex.Message);
+        }
+
+        [Fact]
+        public void UpdatingInvalidTruckFabricationDateShouldThrowException()
+        {
+            var truckToBeUpdated = _truckRepository.FetchAll().First();
+            truckToBeUpdated.AnoFabricacao = "2014";
+
+            var ex = Assert.Throws<Exception>(() => _truckRepository.Update(truckToBeUpdated));
+
+            Assert.Equal("O ano de fabricação deve ser o atual.", ex.Message);
+        }
     }
 }
